747feab287bcda36f7c5950a0777d81e60515f17
ghyyn <91882417+ghyyn@users.noreply.github.com>
2021-12-19 18:01:21 +0800

    Add files via upload

---
+++ contact2.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector(".nav")
+
+window.addEventListener("scroll",function(){
+const scrollTop = window.scrollY
+
+if (scrollTop > 40){
+    nav.classList.add("active")
+
+}
+else{
+    nav.classList.remove("active")
+}
+})
